02664f342f72a58044657ef49f4254305c48eebb
Isak Åslund <aslundisak@gmail.com>
2018-11-12 13:42:03 +0100

    initial project setup

---
+++ src/Uppgift1/Main.java
@@ -0,0 +1,13 @@
+/*
+    Grupp 9
+    Osvald Lindholm & Isak Åslund
+*/
+package Uppgift1;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
